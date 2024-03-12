e1a58dd2fa31fe8312a22a3bcb127260ec7d3e17
zhukings <143058184+zhukings@users.noreply.github.com>
2024-03-13 01:38:38 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -5,11 +5,11 @@ let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，不支持非TLS订阅生成
 let addresses = [
-	'icook.tw:2053#优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+//  'icook.tw:2053#优选域名',
+//	'cloudflare.cfgo.cc#优选官方线路',
 	'cfip.xxxxxxxx.tk:8443#OTC优选',
 	'proxy.xxxxxxxx.tk:8443#OTC反代',
-	'acjp2.cloudflarest.link:2053#JP优选',
+//	'acjp2.cloudflarest.link:2053#JP优选',
 	'yxip.edmero.top:2053#SG优选',
 	
 ];

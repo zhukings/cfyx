7313645f1d3638d5ef7df548d0dada33abe36eb2
zhukings <143058184+zhukings@users.noreply.github.com>
2024-03-23 12:55:38 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -14,13 +14,14 @@ let addresses = [
 // 设置优选地址api接口
 let addressesapi = [
 //	'https://raw.githubusercontent.com/zhukings/cf-rule/main/yxip.txt',
-//  'https://cf-text.pages.dev/bp.txt?token=xy',
-	'https://cf-text.pages.dev/ip.txt?token=xy',
+//  	'https://cf-text.pages.dev/bp.txt?token=xy',
+	'https://cf-text.pages.dev/ip.txt?token=xy'，
+//	'https://cf-text.pages.dev/tc.txt?token=xy',
 
 //	'https://raw.githubusercontent.com/zhukings/vless2sub/main/addressesapi.txt',
-//	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt',
+//  'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt',
 //	'https://addressesapi.090227.xyz/CloudFlareYes',
-//	'https://addressesapi.090227.xyz/ct',
+//  	'https://addressesapi.090227.xyz/ct',
 //	'https://addressesapi.090227.xyz/cmcc',
 ];
 
